c834eade875cd9341c361107750c629822653652
lol <37512400+dimka3553@users.noreply.github.com>
2024-04-17 23:31:44 +0200

    some logic

--- src/algorithms/blockchain.py
+++ src/algorithms/blockchain.py
@@ -3,13 +3,14 @@ import hashlib
 import random
 from collections import defaultdict
 from dataclasses import dataclass
+import time
 
 from ipv8.community import CommunitySettings
 from ipv8.messaging.payload_dataclass import overwrite_dataclass
 from ipv8.types import Peer
 
-from src.da_types import Blockchain, message_wrapper
-from src.merkle_util import merkle_root, merkle_proof
+from da_types import Blockchain, message_wrapper
+from merkle_util import merkle_root, merkle_proof
 
 # We are using a custom dataclass implementation.
 dataclass = overwrite_dataclass(dataclass)
@@ -31,12 +32,42 @@ class Transaction:
 
 
 class Block:
-    def __init__(self, transactions):
-        self.transactions = transactions
-        self.merkle_root = merkle_root([tx.tx_id for tx in transactions])
+    def __init__(self, number, prev_block_time, prev_block_hash, difficulty, target):
+        self.transactions:list[Transaction] = []
+        self.number = number
+        self.hash = 0
+        self.timestamp = 0
+        self.prev_block_hash = prev_block_hash
+        self.difficulty = difficulty
+        self.target = target
+        self.nonce = 0
+        self.hashing_value = ""
 
-    def verify_transaction(self, transaction):
-        return merkle_proof(transaction.tx_id, [tx.tx_id for tx in self.transactions])
+    def get_hashing_value(self):
+        return "".join([tx.tx_id for tx in self.transactions]).join(str(self.nonce))
+        
+
+    def add_transaction(self, transaction: Transaction):
+        if len(self.transactions) < 10:
+            self.transactions.append(transaction)
+        else:
+            pass
+
+    def mine(self):
+        while True:
+            self.hashing_value = self.get_hashing_value()
+            self.hash = hashlib.sha256(self.hashing_value.encode()).hexdigest()
+            if int(self.hash, 16) < self.target:
+                return self.hash
+            self.nonce += 1
+        
+
+
+
+    # def verify_transaction(self, transaction):
+    #     return merkle_proof(transaction.tx_id, [tx.tx_id for tx in self.transactions])
+
+
 
 
 class BlockchainNode(Blockchain):
@@ -50,9 +81,37 @@ class BlockchainNode(Blockchain):
         self.pending_txs = []
         self.finalized_txs = []
         self.balances = defaultdict(lambda: 1000)
+        self.blocks: Block = []
+        self.curr_block = Block(number=0, time=int(time.time()))
+        self.difficulty = 115763819684279741274297652248676021157016744923290554136127638308692447723520
+        self.target_block_time = 10
+        self.puzzle_target = self.calculate_puzzle_target()
+
         #add structure to storing transactions in blocks
         self.key_pair = self.crypto.generate_key("medium")
         self.add_message_handler(Transaction, self.on_transaction)
+
+
+    def create_block(self):
+        self.calculate_difficulty()
+        self.calculate_puzzle_target()
+        self.curr_block = Block(prev_block_hash=self.blocks[-1].hash,
+                                prev_block_time=self.blocks[-1].time,
+                                difficulty= self.difficulty,
+                                target=self.puzzle_target)
+
+    def calculate_difficulty(self):
+        avg_block_time = 0
+        if len(self.blocks) < 100:
+            avg_block_time = (self.blocks[-1].timestamp - self.blocks[0].timestamp) / len(self.blocks)
+        else:
+            avg_block_time = (self.blocks[-1].timestamp - self.blocks[-100].timestamp) / 100
+        self.difficulty = self.difficulty * self.target_block_time / avg_block_time
+
+    def calculate_puzzle_target(self):
+        max_value = hex(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+        return max_value - self.difficulty
+
 
     def sign_transaction(self, transaction: Transaction) -> None:
         transaction.signature = self.crypto.create_signature(self.my_peer.key,
@@ -82,6 +141,14 @@ class BlockchainNode(Blockchain):
             self.cancel_pending_task("tx_create")
             self.stop()
             return
+
+    def process_pending_transactions(self):
+        if len(self.pending_txs) < 10:
+            for txs in self.pending_txs:
+                self.curr_block.add_transaction(txs)
+        else:
+            for i in range(10):
+                self.curr_block.add_transaction(self.pending_txs[i])
 
     def on_start(self):
         if self.node_id % 2 == 0:
@@ -119,15 +186,25 @@ class BlockchainNode(Blockchain):
 
     @message_wrapper(Transaction)
     async def on_transaction(self, peer: Peer, payload: Transaction) -> None:
-
         if self.verify_signature(payload):
             # Add to pending transactions if signature is verified
             print(f'[Node {self.node_id}] Received transaction {payload.nonce} from {self.node_id_from_peer(peer)}')
             if (payload.sender, payload.nonce) not in [(tx.sender, tx.nonce) for tx in self.finalized_txs] and (
                     payload.sender, payload.nonce) not in [(tx.sender, tx.nonce) for tx in self.pending_txs]:
                 self.pending_txs.append(payload)
+                self.curr_block.add_transaction(payload)
         else:
 
             # Gossip to other nodes
             for peer in [i for i in self.get_peers() if self.node_id_from_peer(i) % 2 == 1]:
+                self.ez_send(peer, payload)
+    
+    @message_wrapper(Block)
+    async def on_block(self, peer: Peer, payload: Block) -> None:
+        if self.verify_block(payload):
+            print(f'[Node {self.node_id}] Received block {payload.number} from {self.node_id_from_peer(peer)}')
+            self.blocks.append(payload)
+            self.create_block()
+        else:
+            for peer in [i for i in self.get_peers() if self.node_id_from_peer(i) % 2 == 0]:
                 self.ez_send(peer, payload)
