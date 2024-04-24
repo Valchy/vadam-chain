6e8481922bfd3949ead2b2fe32f0cbec7068706f
Valeri Sabev <valchygaming@gmail.com>
2024-04-24 11:38:09 +0200

    web stuff

--- src/run_local.py
+++ src/run_local.py
@@ -1,41 +1,50 @@
 
 from asyncio import run
+import threading
 
 from ipv8.configuration import ConfigBuilder, default_bootstrap_defs
 from ipv8.util import run_forever, create_event_with_signals
 from ipv8_service import IPv8
 
 from blockchain import BlockchainNode
+from server import run_web_server
 
 from log.logging_config import *
-from utilities.generate_topology import generate_ring_topology, generate_topology
-
-setup_logging()
 
 # Create logger
+setup_logging()
 logger = logging.getLogger('my_app')
+
 async def start_communities(peer_num, use_localhost=True) -> None:
     logger.info('Community started')
     base_port = 9090
-    topology = generate_topology(generate_ring_topology(peer_num), 5)
-    logger.info(f'topology : {topology}')
+    ipv8_instances = {}
+
     for i in range(0, peer_num):
         event = create_event_with_signals()
         node_port = base_port + i
-        connections = topology[i]
+
+        connections = [j for j in range(peer_num) if j != i]
         connections_updated = [(x, base_port + x) for x in connections]
+
         builder = ConfigBuilder().clear_keys().clear_overlays()
         builder.add_key("my peer", "medium", f"ec{i}.pem")
         builder.set_port(node_port)
-        builder.add_overlay("blockchain_community",
-                            "my peer",
-                            [],
-                            default_bootstrap_defs,
-                            {},
-                            [("started", i, connections_updated, event, use_localhost)],
-                            )
-        await IPv8(builder.finalize(),
-                   extra_communities={'blockchain_community': BlockchainNode}).start()
+        builder.add_overlay("blockchain_community","my peer",[],default_bootstrap_defs,{},[("started", i, connections_updated, event, use_localhost)])
+        
+        ipv8_instance = IPv8(builder.finalize(), extra_communities={'blockchain_community': BlockchainNode})
+        await ipv8_instance.start()
+
+        logger.info(f'ADDED NEW INSTANCE {node_port}')
+        ipv8_instances[node_port] = ipv8_instance
+        print(ipv8_instance)
+
+        logger.info(f'Node running on port {node_port}')
+
+    logger.info('OUTSIDE FOR LOOP')
+    # fastapi_thread = threading.Thread(target=lambda: run_web_server(ipv8_instances))
+    # fastapi_thread.start()
+
     await run_forever()
 
-run(start_communities(10))
+run(start_communities(3))
---
+++ src/server.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from log.logging_config import setup_logging
+import logging
+
+# Create logger
+setup_logging()
+logger = logging.getLogger('my_app')
+
+# Create web server
+app = FastAPI()
+
+@app.post("/send-message/{node_port}")
+async def send_message(node_port: int):
+    logger.info(f'Send message api called received, node {node_port}')
+    ipv8_instance = app.ipv8_instances.get(node_port)
+
+    print('IF YOU SEE THIS. THIS WORKED', ipv8_instance.overlays[0].counter)
+    # ipv8_instance.overlays[0].start_client()
+
+    if not ipv8_instance:
+        raise HTTPException(status_code=404, detail="IPv8 instance not found")
+    
+    return {"status": "OK"}
+    
+    # Access the BlockchainNode community from your IPv8 instance
+    blockchain_node = ipv8_instance.overlay['blockchain_community']
+    print(blockchain_node)
+
+    if success:
+        return {"status": "Transaction sent"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to send transaction")
+
+# Host static files
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+def run_web_server(ipv8_instances):
+    app.ipv8_instances = ipv8_instances
+    port = 8000
+
+    logger.info(ipv8_instances)
+
+    while True:
+        try:
+            logger.info(f'Trying to start web server on port {port}...')
+            uvicorn.run(app, host="127.0.0.1", port=port)
+            break  # If the server starts successfully, break the loop
+        except OSError as e:
+            if e.errno == 98:  # Error code for address already in use
+                logger.warning(f"Port {port} is already in use. Trying the next one...")
+                port += 1
+            else:
+                raise  # Reraises any other exception
+
+if __name__ == "__main__":
+    run_web_server()
